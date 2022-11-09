0c7c24ca0ec35244e021ae95f2ba75b59f029d46
ASFahim007 <devasfahim@gmail.com>
2022-11-09 18:02:59 +0600

    single data by id created

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 const { query } = require('express');
@@ -21,11 +21,21 @@ async function run() {
     try{
         const serviceCollection = client.db('metaZone').collection('services')
 
+        // load data from db form services
         app.get('/services', async(req, res) => {
             const query = {};
             const cursor = serviceCollection.find(query);
             const services = await cursor.toArray();
             res.send(services);
+        })
+
+        
+
+        app.get('/services/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const service = await serviceCollection.findOne(query);
+            res.send(service)
         })
 
     }
